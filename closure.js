8dec276b57e48eebe384ca512cf10ec159641c75
sanober riaz <sanober@sanober.me>
2018-01-20 02:29:18 +0000

    automatically backed up by learn

--- closure.js
+++ closure.js
@@ -1,11 +1,11 @@
 function parent() {
   var innerVariable = "Go to your room!";
-  
+
   return function childe() {
     var inaccessible = "**sneaks out the bedroom window**";
     return innerVariable;
   }
-  console.log(inaccessible);
+  console.log(childe());
 }
 
 var outerScope = parent;
@@ -13,5 +13,3 @@ var outerScope = parent;
 var innerScope = outerScope();
 
 innerScope();
-
-
